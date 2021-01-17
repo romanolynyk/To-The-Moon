8036553eacc9bc95ff361de43ba71532637946cb
Roman Olynyk <39267383+romanolynyk@users.noreply.github.com>
2021-01-16 21:27:47 -0500

    Place holder front end added

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,21 @@
+from flask import render_template
 from app import app
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    return render_template('index.html')
+
+@app.route('/pltr')
+def pltr():
+    return render_template('pltr.html')
+
+@app.route('/tsla')
+def tsla():
+    return render_template('tsla.html')
+
+@app.route('/nio')
+def nio():
+    return render_template('nio.html')
+
